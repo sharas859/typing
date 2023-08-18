7b45a8ab9ee42f2732c6701d7a271af316cfc8e6
sharas666 <j.koenig.rsr04@gmx.de>
2023-08-18 12:06:02 +0200

    web

--- typing-11454ead7b601ffe.js
+++ typing-11454ead7b601ffe.js
@@ -180,6 +180,10 @@ function debugString(val) {
     return className;
 }
 
+const CLOSURE_DTORS = new FinalizationRegistry(state => {
+    wasm.__wbindgen_export_2.get(state.dtor)(state.a, state.b)
+});
+
 function makeMutClosure(arg0, arg1, dtor, f) {
     const state = { a: arg0, b: arg1, cnt: 1, dtor };
     const real = (...args) => {
@@ -194,14 +198,14 @@ function makeMutClosure(arg0, arg1, dtor, f) {
         } finally {
             if (--state.cnt === 0) {
                 wasm.__wbindgen_export_2.get(state.dtor)(a, state.b);
-
+                CLOSURE_DTORS.unregister(state)
             } else {
                 state.a = a;
             }
         }
     };
     real.original = state;
-
+    CLOSURE_DTORS.register(real, state, state);
     return real;
 }
 function __wbg_adapter_28(arg0, arg1, arg2) {
